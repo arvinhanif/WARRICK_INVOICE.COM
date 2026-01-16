d9afcd3a2af9b6d90a7e52bf9c8053b8e0837735
arvinhanif <arvinhanif.info@gmail.com>
2026-01-17 05:20:38 +0600

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,17 +1,18 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+}
